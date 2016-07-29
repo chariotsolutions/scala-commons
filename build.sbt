b5609c8417c95be4b8271b1d6f9b0e0111c28d3e
Joe Conley <josephpconley@gmail.com>
2016-07-29 08:47:18 -0400

    initial port of FutureOr utility

---
+++ build.sbt
@@ -0,0 +1,11 @@
+organization := "com.chariotsolutions"
+
+name := "scala-commons"
+
+version := "1.0.0-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+    "org.scalactic" %% "scalactic" % "2.2.4"
+)
---
+++ src/main/scala/com/chariotsolutions/commons/FutureOr.scala
@@ -0,0 +1,48 @@
+package com.chariotsolutions.commons
+
+import org.scalactic._
+import Accumulation._
+
+import scala.collection.GenTraversableOnce
+import scala.collection.generic.CanBuildFrom
+import scala.concurrent.{ExecutionContext, Future}
+
+import scala.languageFeature.higherKinds
+
+case class FutureOr[+A, +E](future: Future[A Or E]) extends AnyVal {
+
+  def flatMap[B, F >: E](f: A => FutureOr[B, F])(implicit ec: ExecutionContext): FutureOr[B, F] = {
+    val newFuture = future.flatMap{
+      case Good(a) => f(a).future
+      case Bad(err) => Future.successful(Bad(err))
+    }
+    FutureOr(newFuture)
+  }
+
+  def map[B](f: A => B)(implicit ec: ExecutionContext): FutureOr[B, E] = FutureOr(future.map(or => or map f))
+
+  def filter[F >: E](f: A => Validation[F])(implicit ec: ExecutionContext): FutureOr[A, F] = FutureOr {
+    future.map { or =>
+      or.filter(f)
+    }
+  }
+
+  final def withFilter[F >: E](p: A => Validation[F])(implicit ec: ExecutionContext): FutureOr[A, F] = filter(p)(ec)
+}
+
+object FutureOr {
+
+  def successful[A, E](a: A): FutureOr[A, E] = FutureOr(Future.successful(Good(a)))
+
+  def bad[A, E](e: E): FutureOr[A, E] = FutureOr(Future.successful(Bad(e)))
+
+  def sequence[A, E, M[+X] <: TraversableOnce[X] with GenTraversableOnce[X]](in: M[Future[A Or Every[E]]])
+  (implicit ec: ExecutionContext,
+  cbfForFutureSequence: CanBuildFrom[M[Future[A Or Every[E]]], A Or Every[E], M[A Or Every[E]]],
+  cbfForCombinable: CanBuildFrom[M[A Or Every[E]], A, M[A]]) = {
+
+    val seq: Future[M[A Or Every[E]]] = Future.sequence(in)
+    val combined: Future[M[A] Or Every[E]] = seq.map(_.combined)
+    FutureOr(combined)
+  }
+}
